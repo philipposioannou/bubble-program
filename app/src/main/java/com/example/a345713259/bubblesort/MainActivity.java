57fb95a1d03714f893f15949accf74a7b9907ef8
philipposioannou <filipposioannou99@gmail.com>
2018-04-07 22:12:33 -0400

    Updating SeekBar

--- app/src/main/java/com/example/a345713259/bubblesort/MainActivity.java
+++ app/src/main/java/com/example/a345713259/bubblesort/MainActivity.java
@@ -19,7 +19,7 @@ import java.util.Scanner;
 // ICS4U1-02
 // */
 
-public class MainActivity extends AppCompatActivity implements OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity {
     SeekBar seekBar1;
 
     int num[];
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements OnSeekBarChangeLi
     Button restartButton;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,114 +43,108 @@ public class MainActivity extends AppCompatActivity implements OnSeekBarChangeLi
         textOutput = findViewById(R.id.text_output);
         stepButton = findViewById(R.id.button_step);
         restartButton = findViewById(R.id.button_reset);
+        changeSpeedSeekBar = findViewById(R.id.seek_bar);
 
-        startPauseButton.setOnClickListener(new View.OnClickListener() {
+        changeSpeedSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int p =0;
             @Override
-            public void onClick(View view) {
-                Thread sortThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        accessFiles();
-                        sortAlgorithm();
-                    }
-                });
-
-                sortThread.start();
-            }
-
-
-            public void restartAlgorithm() {
-
-                restartButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                    }
-                });
-
-            }
-
-
-            public void accessFiles() {
-                // Accessing the testcase file
-                InputStream dataSetFileInputStream = getResources().openRawResource(R.raw.test_case_8);
-                Scanner scanner = new Scanner(dataSetFileInputStream);
-                num = new int[15];
-
-                for (int i = 0; i < num.length; i++) {
-                    num[i] = scanner.nextInt();
-                }
-
-                try {
-                    dataSetFileInputStream.close();
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if(p < 30){
+                    p = 30;
+                    seekBar.setProgress(p);
                 }
             }
 
-
-            public void sortAlgorithm() {
-                int n = num.length;
-                int temp = 0;
-                for (int i = 0; i < n; i++) {
-                    for (int j = 1; j < (n - i); j++) {
-                        if (num[j - 1] > num[j]) {
-                            //swap elements
-
-                            temp = num[j - 1];
-                            num[j - 1] = num[j];
-                            num[j] = temp;
-
-                            //print the results
-                            String result = "";
-                            for (int k = 0; k < num.length; k++) {
-                                result += num[k] + " ";
-                            }
-                            final String Result = result;
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    textOutput.setText(Result);
-                                }
-                            });
-
-                            try {
-                                Thread.sleep(1000);
-                            } catch (InterruptedException ie) {
-
-                            }
-
-                        }
-
-                    }
-                }
-            }
-
-
-            public void onProgressChanged(SeekBar seekBar, int progress,
-                                          boolean fromUser) {
-                seekBarOutput = findViewById(R.id.text_view_seekbar);
-                progressBar = findViewById(R.id.progress_bar);
-                changeSpeedSeekBar = findViewById(R.id.seek_bar);
-                changeSpeedSeekBar.setOnSeekBarChangeListener(this);
-
-
-
+            @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
 
+            @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+                if(p < 30){
+                    p = 30;
+                    seekBar1.setProgress(p);
+                }
             }
 
-            public boolean onCreateOptionsMenu(Menu menu) {
-                return true;
-            }
-        });
+            startPauseButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Thread sortThread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                accessFiles();
+                                sortAlgorithm();
+                            }
+                        });
 
+                        sortThread.start();
+                    }
+
+
+                    public void accessFiles() {
+                        // Accessing the testcase file
+                        InputStream dataSetFileInputStream = getResources().openRawResource(R.raw.test_case_8);
+                        Scanner scanner = new Scanner(dataSetFileInputStream);
+                        num = new int[15];
+
+                        for (int i = 0; i < num.length; i++) {
+                            num[i] = scanner.nextInt();
+                        }
+
+                        try {
+                            dataSetFileInputStream.close();
+                        } catch (IOException ioe) {
+                            ioe.printStackTrace();
+                        }
+                    }
+
+
+                    public void sortAlgorithm() {
+                        int n = num.length;
+                        int temp = 0;
+                        for (int i = 0; i < n; i++) {
+                            for (int j = 1; j < (n - i); j++) {
+                                if (num[j - 1] > num[j]) {
+                                    //swap elements
+
+                                    temp = num[j - 1];
+                                    num[j - 1] = num[j];
+                                    num[j] = temp;
+
+                                    //print the results
+                                    String result = "";
+                                    for (int k = 0; k < num.length; k++) {
+                                        result += num[k] + " ";
+                                    }
+                                    final String Result = result;
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            textOutput.setText(Result);
+                                        }
+                                    });
+
+                                    try {
+                                        Thread.sleep(1000);
+                                    } catch (InterruptedException ie) {
+
+                                    }
+
+                                }
+
+                            }
+                        }
+                    }
+                });
     }
-    }
+}
+
+
+
+
 
 
 
@@ -181,6 +177,16 @@ public class MainActivity extends AppCompatActivity implements OnSeekBarChangeLi
         });
     }
 }
+public void restartAlgorithm() {
+
+                restartButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+
+            }
 
 
 
