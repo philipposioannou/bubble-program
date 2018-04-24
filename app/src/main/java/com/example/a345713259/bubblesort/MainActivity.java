a0edebceebde5753e584f39906d0bc0e4892f746
Philippos Ioannou <38169150+philipposioannou@users.noreply.github.com>
2018-04-24 15:22:27 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/a345713259/bubblesort/MainActivity.java
+++ app/src/main/java/com/example/a345713259/bubblesort/MainActivity.java
@@ -105,3 +105,58 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
+
+
+
+
+int n = num.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (num[j - 1] > num[j]) {
+                    //swap elements
+
+                    temp = num[j - 1];
+                    num[j - 1] = num[j];
+                    num[j] = temp;
+
+                    //print the results
+                    String result = "";
+                    for (int k = 0; k < num.length; k++) {
+                        result += num[k] + " ";
+                    }
+                    final String Result = result;
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textOutput.setText(Result);
+                        }
+                    });
+                    if (paused) {
+                        try {
+                            Thread.sleep(2000);
+
+                        } catch (InterruptedException ie) {
+                        }
+                        //while (!step);{
+                        // step = false;
+
+                    }
+
+                    try {
+                        Thread.sleep(1000);
+
+                    } catch (InterruptedException ie) {
+
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
